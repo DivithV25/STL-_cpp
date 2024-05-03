64014df324429055ec3dab786945ed2a167139d1
DivithV25 <divithv25@gmail.com>
2024-05-03 18:09:02 +0530

    pairs in c++

--- pairs.cpp
+++ pairs.cpp
@@ -1,14 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-void explainPair(){
- pair<int,int> p={1,3};
-cout<<p.first<<" "<<p.second<<endl;
-pair<int,pair<int,int>> p1={1,{3,4}};
-cout<<p1.first<<" "<<p1.second.second<<" "<<p1.second.first<<endl;
-pair<int,int> arr[]={{1,3},{2,5},{5,3}};
-cout<< arr[1].second;
+void explainPair()
+{
+    pair<int, int> p = {1, 3};
+    cout << p.first << " " << p.second << endl;
+    pair<int, pair<int, int>> p1 = {1, {3, 4}};
+    cout << p1.first << " " << p1.second.second << " " << p1.second.first << endl;
+    pair<int, int> arr[] = {{1, 3}, {2, 5}, {5, 3}};
+    cout << arr[1].second;
 }
-int main(){
-  explainPair();
-     
+int main()
+{
+    explainPair();
 }
