c9b9196a26b7f0ad4a20c765f170d9d5c875deac
DivithV25 <divithv25@gmail.com>
2024-05-03 20:13:14 +0530

    queue in c++ STL

---
+++ queue.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void deques(){
+deque<int> dq;
+dq.push_back(1);
+dq.emplace_back(2);
+dq.push_front(4);
+
+dq.pop_back();
+dq.pop_front();
+}
+
+int main(){
+    deques();
+}
