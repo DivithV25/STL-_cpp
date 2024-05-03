d6359a857495b6492cdc2c4554d389661653e37a
DivithV25 <divithv25@gmail.com>
2024-05-03 21:41:29 +0530

    queues in STL c++

---
+++ queue.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+void queues(){
+
+        queue<int> q;
+        q.push(1);
+        q.push(2);
+        q.push(3);
+        q.emplace(4);
+         q.back()+=5;
+         cout<<q.back()<<endl;
+         cout<< q.front()<<endl;
+
+
+}
+int main(){
+    queues();
+}
